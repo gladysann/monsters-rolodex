87f3662692e315c9f6dddc2b27d0a40c8ed6acd2
“Gladys <gatalavera.dev@gmail.com>
2020-08-27 17:28:19 +0800

    Updates

---
+++ precache-manifest.fd2ce1927999becf823125a76722105e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e6dae516688fdcfbd614e94648a4f61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f367420a6def7561877",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bb6dc6d2a89518a2dd64",
+    "url": "/static/js/2.11d22909.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.11d22909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f367420a6def7561877",
+    "url": "/static/js/main.dfd1b0e3.chunk.js"
+  },
+  {
+    "revision": "2013727fa699a94c104c",
+    "url": "/static/js/runtime-main.c475db76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
